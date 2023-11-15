048489762fe5002709168abc2bc9cd53654a8a34
Syauqi <syauqifrizman@gmail.com>
2023-11-16 00:32:48 +0700

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -54,45 +54,4 @@
             </div>
         </div>
     </div>
-
-    {{-- <div class="grid grid-cols-2 gap-4">
-        <div id="left_content" class="">
-
-        </div>
-
-        <div id="right_content" class="w-4/6 h-screen flex flex-col justify-center mx-auto my-auto">
-            <div class="bg-sky-50 rounded border border-solid border-sky-50 pt-5 pr-5 pb-5 pl-5">
-                <div class="flex justify-center items-center">
-                    <h1 class="text-2xl font-medium">Login</h1>
-                </div>
-
-                <form action="" method="post">
-                    <div class="">
-                        <div class="mt-4">
-                            <label for="email" class="mb-2 block">Username/Email</label>
-                            <input required type="email" name="input" id="email" placeholder="Masukkan alamat email anda, e.g. syauqi@gmail.com" pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" class="w-full h-10 rounded pl-4">
-                        </div>
-
-                        <div class="mt-4">
-                            <label for="password" class="mb-2 block">Password</label>
-                            <input required type="password" name="input" id="password" placeholder="Masukkan password anda" class="w-full h-10 rounded pl-4">
-                        </div>
-
-                        <div class="mt-4">
-                            <a href="#" class="underline text-blue">Forgot password?</a>
-                        </div>
-
-                        <div class="mt-4 flex justify-center items-center">
-                            <button type="submit" class="bg-sky-800 text-white rounded-md px-6 py-2 border border-sky-800 hover:bg-sky-700">SIGN IN</button>
-                        </div>
-
-                        <div class="mt-2 flex justify-center items-center">
-                            <p>Don't have an account? <a href="/signup" class="underline text-blue">Sign up</a></p>
-                        </div>
-
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div> --}}
 @endsection
