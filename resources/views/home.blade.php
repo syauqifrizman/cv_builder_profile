4c0b41c0381535910d248b759ca20713908dfd20
Syauqi <syauqifrizman@gmail.com>
2023-11-27 18:12:34 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,7 @@
 @extends('layout.master')
 
 @section('content')
-    <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloremque atque consequatur dolores ab quo nulla, natus eum tempore id magni repellendus doloribus exercitationem autem quod reiciendis! Unde, cum a.
-    </div>
-
-    <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloremque atque consequatur dolores ab quo nulla, natus eum tempore id magni repellendus doloribus exercitationem autem quod reiciendis! Unde, cum a.
+    <div class="h-screen">
+        test
     </div>
 @endsection
