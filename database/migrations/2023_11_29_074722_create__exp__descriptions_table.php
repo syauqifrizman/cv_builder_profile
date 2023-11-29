9e4a67b2e9c25788b5d98d0b74cc7144e20623f6
tastycarbonara <kevinaurelian1@gmail.com>
2023-11-29 14:52:55 +0700

    exp desc fix

--- database/migrations/2023_11_29_074722_create__exp__descriptions_table.php
+++ database/migrations/2023_11_29_074722_create__exp__descriptions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('=Exp_Descriptions', function (Blueprint $table) {
+        Schema::create('Exp_Descriptions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('=Exp_Descriptions');
+        Schema::dropIfExists('Exp_Descriptions');
     }
 };
