3c4a908dd8e3d9be9bfdda847f3751739549def4
tastycarbonara <kevinaurelian1@gmail.com>
2023-11-29 19:44:42 +0700

    document model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Document extends Model
 {
@@ -18,6 +19,31 @@ class Document extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'document_id', 'user_id');
+        return $this->belongsTo(User::class);
+    }
+
+    public function personal(): HasOne
+    {
+        return $this->hasOne(Personal::class);
+    }
+
+    public function education(): HasMany
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function project(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function skill_other(): HasMany
+    {
+        return $this->hasMany(Skill_Other::class);
+    }
+
+    public function experience(): HasMany
+    {
+        return $this->hasMany(Experience::class);
     }
 }
